375227062bd7db825e99fd771a3c9fb63a7cfb6c
Alejo <alejo.sanabrianogueira.1b@gmail.com>
2025-10-31 22:25:26 -0300

    fuerzo ipv4 desde conexion.js

--- backend/modelo/conexion.js
+++ backend/modelo/conexion.js
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import postgres from 'postgres';
+import { setDefaultResultOrder } from 'dns';
+
+// Forzar resolución DNS a IPv4
+setDefaultResultOrder('ipv4first');
 
 const sql = postgres(process.env.DATABASE_URL, {
   ssl: 'require',

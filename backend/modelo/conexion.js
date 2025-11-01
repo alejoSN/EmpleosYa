312f910073b66c260b5fb85fea082c60507d1402
Alejo <alejo.sanabrianogueira.1b@gmail.com>
2025-10-31 21:50:14 -0300

    cambios en el backend para deploy

--- backend/modelo/conexion.js
+++ backend/modelo/conexion.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sql = postgres(process.env.DATABASE_URL, {
-  ssl: { rejectUnauthorized: false }, 
+  ssl: 'require',
 });
 
 export default sql;

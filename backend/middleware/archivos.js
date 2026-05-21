04d394b4893d2e22b2649723fd0e7c6014af6591
Alejo <alejo.sanabrianogueira.1b@gmail.com>
2026-05-21 18:26:31 -0300

    pagina recuperada y funcionando otr vez

--- backend/middleware/archivos.js
+++ backend/middleware/archivos.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_ANON_KEY
 );
 
 const storage = multer.memoryStorage();
